fe04614f4e61e546ff8f190ab6ead5b4aa008c86
MichaelBalcerzak <balcerzak.michael@gmail.com>
2022-10-09 16:07:23 -0400

    CODE start implement the dice class

---
+++ src/main/java/org/courses/comp4004/game/Dice.java
@@ -0,0 +1,46 @@
+package org.courses.comp4004.game;
+
+public class Dice {
+    private String figure;
+    private String[] figures = {"skull", "parrot", "sword", "monkey", "coin", "diamond"};
+    private boolean canRoll = true;
+
+    public Dice() {
+        roll();
+
+    }
+
+    public Dice(String figure) {
+        this.figure = figure;
+    }
+
+    @Override
+    public String toString() {
+        return figure;
+    }
+
+    public String getFigure() {
+        return figure;
+    }
+
+    public void setFigure(String figure) {
+        this.figure = figure;
+    }
+
+    public boolean getCanRoll() {
+        return canRoll;
+    }
+
+    public void setCanRoll(boolean canRoll) {
+        this.canRoll = canRoll;
+    }
+
+    /**
+     * Random dice roll
+     */
+    public void roll() {
+        int a = (int) (Math.random() * 6);
+        figure = figures[(a == 6 ? 5 : a)];
+
+    }
+}
