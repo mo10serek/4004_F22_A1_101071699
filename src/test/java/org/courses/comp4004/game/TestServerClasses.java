f52f10345e8d2502856213147997504294950360
MichaelBalcerzak <balcerzak.michael@gmail.com>
2022-10-09 16:03:50 -0400

    U-TEST test the first and new initialize class called dice to see if it able to roll and set the figure of the dice

---
+++ src/test/java/org/courses/comp4004/game/TestServerClasses.java
@@ -0,0 +1,19 @@
+package org.courses.comp4004.game;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestServerClasses {
+
+    //Dice class
+    //SetDice set a dice that have a face
+    @Test
+    @DisplayName("SetDice")
+    void setDice() {
+        Dice dice = new Dice("sword");
+        Assertions.assertEquals(dice.getFigure(), "sword");
+        dice.setFigure("diamond");
+        Assertions.assertEquals(dice.getFigure(), "diamond");
+    }
+}
