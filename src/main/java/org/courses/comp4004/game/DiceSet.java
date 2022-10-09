d42112b903d3174b1e2edc95c76bed3536f7b12d
MichaelBalcerzak <balcerzak.michael@gmail.com>
2022-10-09 16:36:12 -0400

    CODE make a new class called diceSet

---
+++ src/main/java/org/courses/comp4004/game/DiceSet.java
@@ -0,0 +1,32 @@
+package org.courses.comp4004.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiceSet {
+    final int MAX_SIZE = 8;
+    final boolean USE_STREAMS = true;
+    private List<Dice> diceSet = new ArrayList<>();
+
+    /**
+     * Initialize a set of dice.
+     */
+    public DiceSet() {
+        for (int i = 0; i < MAX_SIZE; i++) {
+            diceSet.add(new Dice());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "" + diceSet;
+    }
+
+    public int getMAX_SIZE() {
+        return MAX_SIZE;
+    }
+
+    public List<Dice> getDiceSet() {
+        return diceSet;
+    }
+}
