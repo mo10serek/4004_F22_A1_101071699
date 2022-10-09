e12c13b489cf4a7bdaf0f760fc5fe9509ebb41bd
MichaelBalcerzak <balcerzak.michael@gmail.com>
2022-10-09 16:46:55 -0400

    CODE - implement the roll and set roll outcome functions

--- src/main/java/org/courses/comp4004/game/DiceSet.java
+++ src/main/java/org/courses/comp4004/game/DiceSet.java
@@ -1,7 +1,9 @@
 package org.courses.comp4004.game;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class DiceSet {
     final int MAX_SIZE = 8;
@@ -32,5 +34,35 @@ public class DiceSet {
 
     public void setDiceSet(List<Dice> diceSet) {
         this.diceSet = diceSet;
+    }
+
+    public void roll() {
+        if (USE_STREAMS) {
+
+            diceSet.stream().
+                    forEach(dice -> dice.roll());
+        } else {
+            for (int i = 0; i < diceSet.size(); i++) {
+                diceSet.get(i).roll();
+            }
+        }
+    }
+
+    public void setRollOutcome(String diceFigures) {
+        String[] faceArray = diceFigures.trim().split("\\s*,\\s*");
+        List<String> faceList = new ArrayList<String>();
+        Collections.addAll(faceList, faceArray);
+
+        if (USE_STREAMS) {
+            int length = faceList.size();
+            IntStream.range(0, length)
+                    .forEach(i -> diceSet.get(i).setFigure(faceList.get(i)));
+        } else {
+
+
+            for (int i = 0; i < faceList.size(); i++) {
+                diceSet.get(i).setFigure(faceList.get(i));
+            }
+        }
     }
 }
