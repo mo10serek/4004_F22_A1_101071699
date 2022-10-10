44987a4e88c825341d99a032d68bb35aff60059e
MichaelBalcerzak <balcerzak.michael@gmail.com>
2022-10-10 09:52:39 -0400

    CODE make a new rule that can check if you rolled 3 skulls, return true if 3 skulls is detected

--- src/main/java/org/courses/comp4004/game/ScoreEvaluator.java
+++ src/main/java/org/courses/comp4004/game/ScoreEvaluator.java
@@ -12,4 +12,18 @@ public class ScoreEvaluator {
         String[] figureArray = diceFigures.trim().split("\\s*,\\s*");
         return figureArray.length < 2 ? new RuleResult(true, 0) : new RuleResult(false, 0);
     }
+
+    /**
+     * Rule checks if 3 skulls are present in the dice set.
+     * Returns true if rule finds that this is the case
+     * @param diceSet
+     * @return
+     */
+    public RuleResult rulePlayerDieIf3Skulls(DiceSet diceSet, FCard fCard){
+        int numberOfSkulls = 0;
+
+        numberOfSkulls += diceSet.countDiceFaces("skull");
+
+        return numberOfSkulls == 3 ? new RuleResult(true, 0) : new RuleResult(false, 0);
+    }
 }
