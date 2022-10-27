514291a2e780038a20081bb7f6ac2d3709c53702
MichaelBalcerzak <balcerzak.michael@gmail.com>
2022-10-27 19:46:27 -0400

    A-TEST implement the test case in row 82

---
+++ src/test/java/org/courses/comp4004/game/Part2Test.java
@@ -0,0 +1,50 @@
+package org.courses.comp4004.game;
+
+import org.junit.jupiter.api.*;
+
+import java.io.*;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Part2Test {
+
+    private static String path = "C:\\Users\\miro\\Documents\\Michael\\Projects\\pirates\\game\\logs";
+
+    PrintStream log;
+
+    @BeforeAll
+    static void preparePaths() throws IOException {
+        Path p = Paths.get(FileSystems.getDefault().getPath("").toAbsolutePath().toString() + File.separator + "logs");
+        if(!Files.exists(p)){
+            path =  Files.createDirectory(p).toString();
+        }else{
+            path = p.toString();
+        }
+    }
+
+    @AfterEach
+    void closeLogFile() throws IOException {
+        log.flush();
+        log.close();
+    }
+
+    // Row82Test	      first roll gets 3 monkeys 3 parrots  1 skull 1 coin  SC = 1100  (i.e., sequence of of 6 + coin)
+    @Test
+    @DisplayName("row82")
+    void row82() {
+        DiceSet diceSet = new DiceSet();
+        FCard fCard = new FCard("Monkey&Parrot", 0);
+        ScoreEvaluator scoreEvaluator = new ScoreEvaluator();
+
+        diceSet.setRollOutcome("monkey, monkey, monkey, parrot, parrot, parrot, skull, coin");
+        Assertions.assertEquals(scoreEvaluator.getScore(fCard, diceSet), 1100);
+        log.println(diceSet);
+        log.println(scoreEvaluator.getScore(fCard, diceSet));
+    }
+
+
+}
