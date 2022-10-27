6f04fbe0f9d849954610d035b318e5e678fac1d0
MichaelBalcerzak <balcerzak.michael@gmail.com>
2022-10-27 14:45:29 -0400

    U-TEST make a test case to test the main class: message Processor that can process the player input command and print to the players what is the outcome which are the dice faces, card faces, player score, and status of the game

--- src/test/java/org/courses/comp4004/game/TestServerClasses.java
+++ src/test/java/org/courses/comp4004/game/TestServerClasses.java
@@ -287,7 +287,7 @@ public class TestServerClasses {
     }
 
     @Test
-    @DisplayName("test Player Destriptor")
+    @DisplayName("test Player Descriptor")
     void testPlayerDescriptor() throws FileNotFoundException {
         PrintWriter out = new PrintWriter("text.txt");
         FileReader fr =new FileReader("text.txt");
@@ -304,6 +304,22 @@ public class TestServerClasses {
         playerDescriptor.setDrawnFCard(drawnFCard);
         Assertions.assertEquals(playerDescriptor.getDrawnFCard(), drawnFCard);
         Assertions.assertEquals(playerDescriptor.getDiceSet(), diceSet);
+    }
+
+    @Test
+    @DisplayName("messageProcessor")
+    void testMessageProcessor() {
+        FCardDeck fCardDeck = new FCardDeck();
+        DiceSet diceSet = new DiceSet();
+        ScoreEvaluator scoreEvaluator = new ScoreEvaluator();
+        LineParser lineParser = new LineParser();
+        List<PlayerDescriptor> playerDescriptorList = new ArrayList<>();
+
+        MessageProcessor messageProcessor = new MessageProcessor(fCardDeck, diceSet, scoreEvaluator, lineParser,
+                playerDescriptorList);
+
+        PostStatus toReturn = messageProcessor.ProcessMessage(" ");
+        Assertions.assertEquals(toReturn.outMsg, "unknown command");
     }
 
 
