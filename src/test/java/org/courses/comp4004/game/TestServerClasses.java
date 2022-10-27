942dfeb8c2477e974301e0cc3cbf5d96072db576
MichaelBalcerzak <balcerzak.michael@gmail.com>
2022-10-27 17:22:26 -0400

    U-TEST test a new command which is mode Broadcasting

--- src/test/java/org/courses/comp4004/game/TestServerClasses.java
+++ src/test/java/org/courses/comp4004/game/TestServerClasses.java
@@ -339,5 +339,22 @@ public class TestServerClasses {
         Assertions.assertEquals(toReturn.outMsg, Commands.modeInteracting);
     }
 
+    @Test
+    @DisplayName("MessageServerBroadcasting")
+    void testModeServerBroadcasting() {
+        FCardDeck fCardDeck = new FCardDeck();
+        DiceSet diceSet = new DiceSet();
+        ScoreEvaluator scoreEvaluator = new ScoreEvaluator();
+        LineParser lineParser = new LineParser();
+        List<PlayerDescriptor> playerDescriptorList = new ArrayList<>();
+
+        MessageProcessor messageProcessor = new MessageProcessor(fCardDeck, diceSet, scoreEvaluator, lineParser,
+                playerDescriptorList);
+
+        PostStatus toReturn = messageProcessor.ProcessMessage(Commands.modeServerBroadcasting);
+        Assertions.assertTrue(toReturn.success);
+        Assertions.assertEquals(toReturn.outMsg, Commands.modeServerBroadcasting);
+    }
+
 
 }
