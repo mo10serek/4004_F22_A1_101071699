01f1f97701e9afff9bc29f8dad4663d5e845c4a0
MichaelBalcerzak <balcerzak.michael@gmail.com>
2022-10-27 13:29:42 -0400

    CODE - create the init function

--- src/main/java/org/courses/comp4004/game/LineParser.java
+++ src/main/java/org/courses/comp4004/game/LineParser.java
@@ -1,6 +1,10 @@
 package org.courses.comp4004.game;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static java.util.Objects.isNull;
 
 public class LineParser { private String line;
 
@@ -9,6 +13,35 @@ public class LineParser { private String line;
     private List<String> parmsList= null;
 
     public LineParser() {
+    }
+
+    public void init(String line){
+        this.line = line;
+        cmd = "";
+        parmsLine = "";
+        String parmsLineParsed = "";
+        parmsList = new ArrayList<String>();
+
+        if(!isNull(this.line)){
+            String[] splitCmd = this.line.trim().split("\\s+",2);
+            if(!isNull(splitCmd[0])) {
+                cmd = splitCmd[0];
+            }
+            if((splitCmd.length == 2) && (!isNull(splitCmd[1]))) {
+                parmsLine = splitCmd[1];
+            }
+
+            if((splitCmd.length > 1) && (!isNull(splitCmd[1]))) {
+                for(int i=1; i< splitCmd.length; i++) {
+                    parmsLineParsed = parmsLineParsed + splitCmd[i];
+                }
+                if(parmsLineParsed.lastIndexOf(',') == parmsLineParsed.length()-1){
+                    parmsLineParsed = parmsLineParsed.substring(0,parmsLineParsed.length()-1);
+                }
+                String[] splitParmsArray = parmsLineParsed.trim().split("\\s*,+\\s*");
+                Collections.addAll(parmsList, splitParmsArray);
+            }
+        }
     }
 
     public String getLine() {
