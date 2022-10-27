9efa0621fb4f52777040f64c9d04643c985779ca
MichaelBalcerzak <balcerzak.michael@gmail.com>
2022-10-27 18:25:05 -0400

    CODE implement a roll function to select the faces you want to roll

--- src/main/java/org/courses/comp4004/game/DiceSet.java
+++ src/main/java/org/courses/comp4004/game/DiceSet.java
@@ -3,7 +3,11 @@ package org.courses.comp4004.game;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static java.util.Objects.isNull;
 
 public class DiceSet {
     final int MAX_SIZE = 8;
@@ -155,5 +159,60 @@ public class DiceSet {
         diceSet.stream().
                 forEach(dice -> dice.setCanRoll(false));
 
+    }
+
+    /**
+     * It is used for the first roll;
+     * 1. Each dice has a flag that disable (or enable rolling); this method first test if parameters are correct
+     * and then enables flag for all dice. (There can be disabled dices from previous player rolls.
+     * @param diceFigures
+     * @return
+     */
+    public String roll(String diceFigures) {
+        final String[] status = {"" };
+        String[] figureToRoll = {""};
+        String[] figureArray = diceFigures.trim().split("\\s*,\\s*");
+        List<String> figureList = new ArrayList<String>();
+        Collections.addAll(figureList, figureArray);
+
+        if (USE_STREAMS) {
+            Map<String, Long> figureCount = figureList.stream().collect(Collectors.groupingBy(figure -> figure, Collectors.counting()));
+            Map<String, Long> figureDiceSetCount = diceSet.stream().collect(Collectors.groupingBy(dice -> dice.getFigure(), Collectors.counting()));
+            figureCount.forEach((figure, count) -> {
+                // Check counts
+                long availableCount = isNull(figureDiceSetCount.get(figure)) ? 0 : figureDiceSetCount.get(figure);
+                if (count > availableCount) {
+                    // Error: Try to roll more figures than available in the dice set
+                    status[0] = status[0] + "request roll " + Long.valueOf(count).intValue() + " " + figure + " available "
+                            + Long.valueOf(availableCount).intValue() + ", ";
+                }
+            });
+            // If status is empty the roll can be done otherwise return status
+
+            if (status[0].length() == 0) {  //no errors
+                //Enable roll of all dies that could be skull from the previous player
+                enableAllDiceRoll();
+
+                diceSet.stream().
+                        forEach(dice -> {
+                            //Returns: This method returns new remapped value associated with the specified key, or null if mapping returns null.
+                            figureToRoll[0] = dice.getFigure();
+                            if ( !isNull(figureCount.get(dice.getFigure())) && figureCount.get(dice.getFigure()).intValue() != 0) {
+                                if (!isNull(figureCount.computeIfPresent(dice.getFigure(), (key, oldValue) -> oldValue - 1))) {
+                                    if(dice.getCanRoll() && !dice.getCanHold()) {
+                                        dice.roll();
+                                        dice.disableRollIfFigure("skull");
+                                    }
+                                }
+                            }
+                        });
+            }
+        }
+
+        int sLength = status[0].length();
+        if (sLength != 0 && (status[0].substring(sLength - 2, sLength)).equals(", ")) {
+            status[0] = status[0].substring(0, sLength - 2);
+        }
+        return status[0].length() != 0 ? status[0] = "Error: " + status[0] : "";
     }
 }
