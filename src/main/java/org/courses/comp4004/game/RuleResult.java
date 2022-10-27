abd6f528d50bf2fa6d4c648330d5ab04b21fca63
MichaelBalcerzak <balcerzak.michael@gmail.com>
2022-10-27 12:33:27 -0400

    CODE - implement the parameter message and put setters and getters

--- src/main/java/org/courses/comp4004/game/RuleResult.java
+++ src/main/java/org/courses/comp4004/game/RuleResult.java
@@ -3,10 +3,17 @@ package org.courses.comp4004.game;
 public class RuleResult {
     private boolean pass;
     private int score;
+    private String message = "";
 
     public RuleResult(boolean pass, int score) {
         this.pass = pass;
         this.score = score;
+    }
+
+    public RuleResult(boolean pass, int score, String message) {
+        this.pass = pass;
+        this.score = score;
+        this.message = message;
     }
 
     public boolean isPass() {
@@ -25,8 +32,16 @@ public class RuleResult {
         this.score = score;
     }
 
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
     @Override
     public String toString() {
-        return "" + pass + "  " + score;
+        return "" + pass + ", =" + score + ", " + message;
     }
 }
